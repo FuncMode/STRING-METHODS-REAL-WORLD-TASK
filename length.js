0bad25490687a4e3e0cf283bbf377d560a17d8e3
Angelito Tallod <angelitotallod1234@gmail.com>
2025-10-30 09:05:23 +0800

    real world task using string methods

---
+++ length.js
@@ -0,0 +1,28 @@
+// let str = 'practice'
+// let num = "2"
+// console.log('hello'.length);
+// console.log(str.charAt(0));
+// console.log(str.concat(" " + "demo"));
+// console.log(str.at(-1));
+// console.log(str[0]);
+// console.log(str.slice(1, 2));
+// console.log(str.slice(str.length - 1))
+// console.log(str.substring(0, 1));
+// console.log(str.substr(0, 2));
+// console.log(num.padStart(3, "2"));
+
+// console.log(str.repeat(3))
+// console.log(str.replace('pra', 'kupal'));
+// console.log(str.split(""));
+// console.log(str.indexOf('c', 4));
+
+// let text = "Please locate locate";
+
+// console.log(text.lastIndexOf('locate', 1));
+// console.log(text.search('locate'));
+
+// let text = "The rain in Spain";
+
+// for (let t of text.match(/ain/g)) {
+//     console.log(t);
+// }
